6c77506e920253b042be79a6bd4e790b5d76b1eb
lexk <kutsenko.new@gmail.com>
2019-03-21 00:19:40 +0200

    Make endpoint with healthcheck

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 
 import App from './App.vue'
+import axios from './axios.vue'
 
 Vue.config.productionTip = false
 Vue.use(VueRouter)
@@ -9,3 +10,7 @@ Vue.use(VueRouter)
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+new Vue({
+	render: h=> h(axios),
+}).$mount('#axios')
