d079b33d47524a8f0893273274cf30a00bb2a716
anastasiia-l <anastasiia.leshkevych@nure.ua>
2019-04-11 17:39:43 +0300

    Add assets and Footer component

---
+++ src/components/Footer/Footer.vue
@@ -0,0 +1,30 @@
+<template>
+    <footer class="blog-footer">
+        <p>Blog template built for <a href="https://getbootstrap.com/">Bootstrap</a> by <a
+                href="https://twitter.com/mdo">@mdo</a>.</p>
+        <p>
+            <a href="#">Back to top</a>
+        </p>
+    </footer>
+</template>
+
+<script>
+    export default {
+        name: "Footer"
+    }
+</script>
+
+<style scoped>
+    .blog-footer {
+        padding: 2.5rem 0;
+        color: #999;
+        text-align: center;
+        background-color: #f9f9f9;
+        border-top: .05rem solid #e5e5e5;
+    }
+
+    .blog-footer p:last-child {
+        margin-bottom: 0;
+    }
+
+</style>
